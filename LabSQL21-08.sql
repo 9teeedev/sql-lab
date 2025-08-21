c6fca02352a59881ae93023fa7023194b26c28d6
9teeedev <teezeezax2@gmail.com>
2025-08-21 09:27:38 +0700

    Update file

--- LabSQL21-08.sql
+++ LabSQL21-08.sql
@@ -6,3 +6,14 @@ SELECT CategoryName, ProductName, UnitPrice FROM Products JOIN Categories
 ON Products.CategoryID = Categories.CategoryID
 ORDER BY CategoryName
 
+SELECT CategoryName, ProductName, UnitPrice FROM Products AS P JOIN Categories AS C
+ON P.CategoryID = C.CategoryID
+ORDER BY CategoryName
+
+SELECT CompanyName, OrderID FROM Orders, Shippers
+WHERE Shippers.ShipperID = Orders.ShipVia
+ORDER BY OrderID
+
+SELECT CompanyName, OrderID FROM Orders JOIN Shippers
+ON Shippers.ShipperID = Orders.ShipVia
+ORDER BY OrderID
